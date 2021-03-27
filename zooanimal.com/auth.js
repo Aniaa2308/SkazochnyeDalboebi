c20e342453b89e3a00d9df8b25cd28fa71aa360b
RomaniukDenis <den.romaniuk.2010@gmail.com>
2021-03-27 14:13:19 +0200

    zoo

--- zooanimal.com/auth.js
+++ zooanimal.com/auth.js
@@ -1,4 +1,4 @@
-var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)ava2\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)ava\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
 if(cookieValue != ""){
     let auth = document.getElementById('auth');
--- zooanimal.com/script.js
+++ zooanimal.com/script.js
@@ -1,40 +1,40 @@
 const url = "https://my-json-server.typicode.com/Aniaa2308/SkazochnyeDalboebi/animals/"
 
-$('#poisk').click(()=>{
-    let query = $('#vv').val();
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url + query)
+// $('#poisk').click(()=>{
+//     let query = $('#vv').val();
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url + query)
     
-        xhr.onload = () => {
-            if(xhr.status > 300){
-                console.log(xhr.status);
-            }
-            let res = JSON.parse(xhr.response);
-                $('#result').html(`
-                    <h1>${res.name}</h1>
-                    <div>${res.description}</div>
-                    <img src="${res.avatar_url}" width="200px">
-                `);
+//         xhr.onload = () => {
+//             if(xhr.status > 300){
+//                 console.log(xhr.status);
+//             }
+//             let res = JSON.parse(xhr.response);
+//                 $('#result').html(`
+//                     <h1>${res.name}</h1>
+//                     <div>${res.description}</div>
+//                     <img src="${res.avatar_url}" width="200px">
+//                 `);
          
-        }
+//         }
     
-        xhr.onerror = (err) => {
-            console.log(err);
-        }
+//         xhr.onerror = (err) => {
+//             console.log(err);
+//         }
     
-        xhr.send();
-    }
-);
+//         xhr.send();
+//     }
+// );
 
 
 const url1 = "https://my-json-server.typicode.com/Aniaa2308/SkazochnyeDalboebi/users"
 
 function login(){
     let login = document.getElementById('login').value;
-    let password = document.getElementById('password');
+    let password = document.getElementById('password').value;
     let xhr1 = new XMLHttpRequest();
 
-    xhr1.open('GET',url1);
+    xhr1.open('GET',"https://my-json-server.typicode.com/Aniaa2308/SkazochnyeDalboebi/users");
 
     xhr1.onload = () => {
         let users = JSON.parse(xhr1.response);
